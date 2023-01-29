c99e92948acc07ae9b91a220cef066caee27c650
Kuzmych Roman <67451349+Dobriyr@users.noreply.github.com>
2023-01-29 03:32:55 +0200

    Add styles

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -14,6 +14,7 @@ var app = builder.Build();
 //app.MapGet("/", () => "Hello World!");
 
 app.UseStaticFiles();
+app.MapControllerRoute("pagination", "Products/Page{productPage}", new { Controller = "Home", action = "Index" });
 app.MapDefaultControllerRoute();
 
 SeedData.EnsurePopulated(app);
