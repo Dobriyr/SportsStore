741bcaa53cc629a81ab2b1713915b7ff492e0eb4
Kuzmych Roman <67451349+Dobriyr@users.noreply.github.com>
2023-02-10 12:29:10 +0200

    Add crud functionality and Admin razor pages

--- SportsStore/Models/Product.cs
+++ SportsStore/Models/Product.cs
@@ -1,15 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SportsStore.Models
 {
 	public class Product
 	{
 		public long? ProductID { get; set; }
+		[Required(ErrorMessage = "Please enter product name")]
 		public string Name { get; set; } = String.Empty;
+		[Required(ErrorMessage = "Please enter product descriprion")]
 		public string Description { get; set; } = String.Empty;
 
+		[Required]
+		[Range(0.01, double.MaxValue, ErrorMessage ="Please enter positive price")]
 		[Column(TypeName ="decimal(8,2)")]
 		public decimal Price { get; set; }
+		[Required(ErrorMessage = "Please specify a category")]
 		public string Category { get; set; } = String.Empty;
 	}
 }
--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -18,6 +18,8 @@ builder.Services.AddSession();
 builder.Services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
+builder.Services.AddServerSideBlazor();
+
 var app = builder.Build();
 //app.MapGet("/", () => "Hello World!");
 
@@ -42,6 +44,10 @@ app.MapControllerRoute("pagination",
 
 app.MapDefaultControllerRoute();
 app.MapRazorPages();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/admin/{*catchall}", "/Admin/Index");
+
 
 SeedData.EnsurePopulated(app);
 app.Run();
